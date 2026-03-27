8f0ad83aa45c1ec57f927da89e3598e2a04fc887
Samarth <samarth@Samarths-MacBook-Air-2.local>
2026-03-27 17:06:11 +0530

    sixth Update

--- main.py
+++ main.py
@@ -125,8 +125,8 @@ def run():
     res = greedy(problem, h1)
     print_timetable(res.solution, rooms, problem.slots, problem)
 
-    print("\nA* (h1)")
-    res = astar(problem, h1)  # using fast heuristic
+    print("\nA* (h2)")
+    res = astar(problem, h2)  # using fast heuristic
     print_timetable(res.solution, rooms, problem.slots, problem)
 
     print("\nUCS")
